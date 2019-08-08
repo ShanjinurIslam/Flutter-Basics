ed5340713061a25bb6ea5c39452e64b4522ee2cc
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-08-08 22:07:10 +0600

    tutorial - 8

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -13,8 +13,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHome extends StatelessWidget {
-  List<String> items = ['a', 'b', 'c', 'd', 'e'];
+class MyHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyHomeState();
+  }
+}
+
+class MyHomeState extends State<MyHome> {
+  bool flag = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,9 +57,53 @@ class MyHome extends StatelessWidget {
                           .50,
                       child: ListView.builder(
                         itemBuilder: (BuildContext context, int index) {
-                          return Text(items[index]);
+                          return Container(
+                            height: MediaQuery.of(context).size.height *
+                                (60.0 / 948.0),
+                            width: MediaQuery.of(context).size.width,
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        flag = !flag;
+                                      });
+                                    },
+                                    child: flag
+                                        ? Image.asset('asset/icons/Marked.png')
+                                        : Image.asset(
+                                            'asset/icons/Unmarked.png'),
+                                  ),
+                                  top: MediaQuery.of(context).size.height *
+                                      (16.0 / 948.0),
+                                  left: MediaQuery.of(context).size.width *
+                                      (16.0 / 375.0),
+                                ),
+                                Positioned(
+                                  top: MediaQuery.of(context).size.height *
+                                      (19.0 / 948.0),
+                                  left: MediaQuery.of(context).size.width *
+                                      (60.0 / 375.0),
+                                  child: Text(
+                                    'Start Making a Presentation',
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: MediaQuery.of(context).size.height *
+                                      (24.0 / 948.0),
+                                  left: MediaQuery.of(context).size.width *
+                                      (347.0 / 375.0),
+                                  child: Image.asset('asset/icons/work.png'),
+                                ),
+                              ],
+                            ),
+                          );
                         },
-                        itemCount: items.length,
+                        itemCount: 1,
                       ),
                     ),
                     Container(
