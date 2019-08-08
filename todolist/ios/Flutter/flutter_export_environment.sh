8be44546ef1d7892155c1bf73799a5336594f916
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-08-08 20:32:56 +0600

    tutiorial part 2

---
+++ todolist/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/spondon/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/PortableSSD/Projects/Development/Flutter-Tutorials/todolist"
+export "FLUTTER_TARGET=/Volumes/PortableSSD/Projects/Development/Flutter-Tutorials/todolist/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/spondon/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
